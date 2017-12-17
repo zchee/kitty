b65e869ffbd8c808e5f851f6b0f1cef9a4428faa
Koichi Shiraishi <zchee.io@gmail.com>
2017-12-17 15:13:13 +0900

    decrease cell_width for Source Han Code JP font

--- kitty/core_text.m
+++ kitty/core_text.m
@@ -244,7 +244,7 @@ cell_metrics(PyObject *s, unsigned int* cell_width, unsigned int* cell_height, u
             if (w > width) width = w;
         }
     }
-    *cell_width = MAX(1, width);
+    *cell_width = MAX(1, width) - 1;
     *underline_position = floor(self->ascent - self->underline_position + 0.5);
     *underline_thickness = (unsigned int)ceil(MAX(0.1, self->underline_thickness));
     *baseline = (unsigned int)self->ascent;
